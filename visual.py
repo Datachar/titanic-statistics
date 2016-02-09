57200fe9a1b451480189d00ec711da343e4fc94c
ghost <myhailo.mykytyn.96@mail.ru>
2016-02-09 21:41:30 +0200

    visual

--- visual.py
+++ visual.py
@@ -1,6 +1,50 @@
 import sys
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import QApplication, QWidget
 
-app = QApplication(sys.argv)
-button = Q
+from PyQt5.QtWidgets import (QApplication, QWidget, QToolTip, QPushButton)
+from PyQt5.QtGui import QFont
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUi()
+
+    def initUi(self):
+       # QToolTip.setFont(QFont('SansSerif', 10))
+
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Tooltips')
+        self.show()
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+'''
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = QWidget()
+    window.resize(250, 150)
+    window.move(300,300)
+    window.setWindowTitle('Simple')
+
+    label =
+
+
+    window.show()
+
+    sys.exit(app.exec_())
+    '''
