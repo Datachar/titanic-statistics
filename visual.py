6e96cea7aef25621e239821701fb3d1e68142339
ghost <myhailo.mykytyn.96@mail.ru>
2016-02-11 21:52:56 +0200

    edit visual

--- visual.py
+++ visual.py
@@ -3,9 +3,13 @@ import sys
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout, QTextBrowser, QLineEdit, QGridLayout, QPushButton, QLayout
 from PyQt5.QtGui import QTextLayout
 
-from passenger import Passenger
+from passenger import Passengers
 #from utils import create_analysis_widget
+from pandas import DataFrame, read_csv
+import pandas as pd
 
+df = pd.read_csv(r'titanic.csv')
+pas = Passengers(df)
 
 class TitanicStatApp:
     def __init__(self):
@@ -21,48 +25,32 @@ class TitanicStatApp:
         self.most_popular_female_name = None
         self.prob_of_surviving = None
         self.form_layout = None
+        self.main_layout = None
+        self.analysis_layout = None
 
     def run(self):
         app = QApplication(sys.argv)
 
         self.main_windows = QWidget()
-
+        self.main_layout = QVBoxLayout()
         self.main_windows.setWindowTitle('title')
         self.main_windows.resize(400, 400)
-        # self.main_layout = QTextLayout()
-
-        self.sum_male = QLabel(get_sum_male('Amount male'), self.main_windows)
-        self.sum_male.move(20, 20)
-
-        self.sum_female = QLabel(get_sum_female('Amount female'), self.main_windows)
-        self.sum_female.move(20, 40)
-
-        self.percent_of_surviving = QLabel(get_percent_of_survived('Percent of first class passengers'), self.main_windows)
-        self.percent_of_surviving.move(20, 60)
-
-        self.age_mean = QLabel(get_age_mean('Mean age of passengers'), self.main_windows)
-        self.age_mean.move(20, 80)
-
-        self.age_median = QLabel(get_age_median('Median age of passengers'), self.main_windows)
-        self.age_median.move(20, 100)
 
         self.most_popular_female_name = None
 
-        self.form_layout = self.prob_of_surv()
-      #  self.form_layout.setDefaultPositioning()
-        self.main_windows.setLayout(self.form_layout)
-       # self.main_windows.unsetLayoutDirection()
-       # self.main_windows.
+        self.form_layout = self.probably_of_static()
+        self.analysis_layout = self.analysis_data_show()
+
+        self.main_layout.addLayout(self.analysis_layout)
+        self.main_layout.addLayout(self.form_layout)
+
+
+
+        self.main_windows.setLayout(self.main_layout)
         self.main_windows.show()
         sys.exit(app.exec_())
 
-    '''
-    def btn_action(self):
-        btn = QPushButton('ok')
-        btn.clicked.connect()
-    '''
-
-    def prob_of_surv(self):
+    def probably_of_static(self):
         age = QLabel('Age')
         klass = QLabel('Class')
         gender = QLabel('Gender')
@@ -72,8 +60,7 @@ class TitanicStatApp:
         genderEdit = QLineEdit()
 
         grid = QGridLayout()
-        grid.activate()
-        grid.setSpacing(10)
+        grid.setSpacing(5)
 
         grid.addWidget(klass, 1, 0)
         grid.addWidget(klassEdit, 1, 1)
@@ -86,30 +73,51 @@ class TitanicStatApp:
 
         return grid
 
+    def analysis_data_show(self):
+        grid = QGridLayout()
+        grid.setSpacing(5)
+
+        sum_male = QLabel(get_sum_male('Amount male'))
+        grid.addWidget(sum_male, 1,0)
+
+        sum_female = QLabel(get_sum_female('Amount female'))
+        grid.addWidget(sum_female, 2,0)
+
+        percent_of_surviving = QLabel(get_percent_of_surviving('Percent of first class passengers'))
+        grid.addWidget(percent_of_surviving, 3,0)
+
+        age_mean = QLabel(get_age_mean('Mean age of passengers'))
+        grid.addWidget(age_mean)
+
+        age_median = QLabel(get_age_median('Median age of passengers'))
+        grid.addWidget(age_median)
+
+        return grid
+
 
 def get_sum_male(text):
-    res = text + ': ' + str(Passenger.sum_male())
-    return res
+    res = text + ': ' + str(pas.sum_male())
+    return str(res)
 
 
 def get_sum_female(text):
-    res = text + ': ' + str(Passenger.sum_female())
-    return res
+    res = text + ': ' + str(pas.sum_female())
+    return str(res)
 
 
-def get_percent_of_survived(text):
-    res = text + ': ' + str(Passenger.per_of_survived())
-    return res
+def get_percent_of_surviving(text):
+    res = text + ': ' + str(pas.percent_of_surviving())
+    return str(res)
 
 
 def get_age_mean(text):
-    res = text + ': ' + str(Passenger.age_mean())
-    return res
+    res = text + ': ' + str(pas.mean_age_of_passenger())
+    return str(res)
 
 
 def get_age_median(text):
-    res = text + ': ' + str(Passenger.age_median())
-    return res
+    res = text + ': ' + str(pas.median_age_of_passenger())
+    return str(res)
 
 # def get_probability_of_surviving(text, )
 
